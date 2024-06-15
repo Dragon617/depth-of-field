64592d4a229d7c3db90e158ffc7bc741965395d5
Dragon0617 <103638567+Dragon617@users.noreply.github.com>
2024-06-16 00:33:04 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -210,12 +210,12 @@ function App() {
         <Box pt={6}>
           <Flex gap={2}>
             <Box w="20%">
-              <Text align="right">Units</Text>
+              <Text align="right">单位</Text>
             </Box>
 
             <Box flexGrow={1}>
               <RadioGroup
-                onChange={(v) => setSystem(v as "Imperial" | "Metric")}
+                onChange={(v) => setSystem(v as "英寸" | "米")}
                 value={system}
               >
                 <Stack direction="row">
@@ -234,7 +234,7 @@ function App() {
           <Flex gap={2}>
             <Box w="20%">
               <Text align="right">
-                Subject Distance ({system === "Imperial" ? "ft" : "m"})
+                受试者距离 ({system === "Imperial" ? "ft" : "m"})
               </Text>
             </Box>
             <Box flexGrow={1}>
@@ -263,7 +263,7 @@ function App() {
         <Box pt={6}>
           <Flex gap={2}>
             <Box w="20%">
-              <Text align="right">Focal Length (mm)</Text>
+              <Text align="right">焦距（mm）</Text>
             </Box>
             <Box flexGrow={1}>
               <Slider
@@ -304,7 +304,7 @@ function App() {
         <Box pt={6}>
           <Flex gap={2}>
             <Box w="20%">
-              <Text align="right">Aperture</Text>
+              <Text align="right">光圈</Text>
             </Box>
             <Box flexGrow={1}>
               <Slider
@@ -333,7 +333,7 @@ function App() {
           <Flex gap={2}>
             <Flex gap={2} width="50%">
               <Box w="20%" mt={2}>
-                <Text align="right">Sensor Size</Text>
+                <Text align="right">传感器尺寸</Text>
               </Box>
               <Box flexGrow={1}>
                 <Select
@@ -357,7 +357,7 @@ function App() {
 
             <Flex gap={2} width="50%">
               <Box w="20%" mt={2}>
-                <Text align="right">Subject</Text>
+                <Text align="right">主题</Text>
               </Box>
               <Box flexGrow={1}>
                 <Select
@@ -400,7 +400,7 @@ function App() {
 
         <Box p={4} pt={6}>
           <Flex gap={5} justify="center">
-            <a href="https://github.com/jherr/depth-of-field" target="_blank">
+            <a href="https://github.com/Dragon617/depth-of-field" target="_blank">
               Contribute to this open source project on GitHub.
             </a>
           </Flex>
